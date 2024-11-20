bc523e1f68da446c605fa0f30e0e3b413111db3c
Alexandre-Ribes <aribes64@gmail.com>
2024-11-20 16:35:26 +0100

    Ajout de la structure projet + début premiere fonctionnalité

--- BiblioFind.Cmd/Program.cs
+++ BiblioFind.Cmd/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace BiblioFind.Cmd
+﻿using BiblioFind.Data.Repositories;
+using System.Text;
+
+namespace BiblioFind.Cmd
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            IBookRepository bookRepository = new ApiBookRepository("http://localhost:5253");
             Console.WriteLine("Hello, World!");
+            ListBookShelf(bookRepository);
+            Console.ReadLine();
+
+
+        }
+
+        private static void ListBookShelf(IBookRepository bookRepository)
+        {
+            Console.WriteLine("Entrez l'id de la catégorie");
+            if (int.TryParse(Console.ReadLine(), out int id))
+            {
+
+                var books = bookRepository.GetShelf(id).Result;
+                if (books != null)
+                {
+                    StringBuilder message = new StringBuilder();
+                    foreach (var book in books)
+                    {
+                        message.Append($"{book.Title}\n");
+                    }
+                    Console.WriteLine(message.ToString());
+                }
+
+                else
+                {
+
+                    Console.WriteLine("Erreur : aucune catégorie trouvée avec cet ID.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Erreur : ID invalide.");
+
+
+            }
         }
     }
 }
--- BiblioFind.Data/DataContext.cs
+++ BiblioFind.Data/DataContext.cs
@@ -1,7 +1,39 @@
-﻿namespace BiblioFind.Data
-{
-    public class DataContext
-    {
+﻿using BiblioFind.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace BiblioFind.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DbSet<AuthorModel> Author { get; set; }
+        public DbSet<BookModel> Book { get; set; }
+
+        public DbSet<ShelfModel> Shelf { get; set; }
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated(); 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AuthorModel>().HasData(
+                new AuthorModel() { Id = 1, FirstName = "JK", Name = "Rowlling"},
+                new AuthorModel() { Id = 2, FirstName = "JRR", Name = "Tolkien" },
+                new AuthorModel() { Id = 3, FirstName = "RR", Name = "Martin" }
+            );
+
+            modelBuilder.Entity<BookModel>().HasData(
+                new BookModel() { Id = 1, Title = "HP1", Status = false, AuthorModelId=1,ShelfModelId = 1 },
+                new BookModel() { Id = 2, Title = "HP2", Status = false, AuthorModelId=1, ShelfModelId = 1 },
+                new BookModel() { Id = 3, Title = "Seigneurs des anneaux", Status = false, AuthorModelId=2, ShelfModelId = 1 }
+                );
+
+            modelBuilder.Entity<ShelfModel>().HasData(
+                new ShelfModel() { Id = 1 , Name = "Fantastique"}
+            );
+        }
     }
 }
